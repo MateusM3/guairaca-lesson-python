8c20cdf21872774fa68f488a411fde289cf4c1c9
Mateus Monteiro <mateusmonteirom3@gmail.com>
2025-02-10 22:59:24 -0300

    first commit

---
+++ script.py
@@ -0,0 +1,16 @@
+pedido = ['mouse', 'teclado', 'monitor']
+contador = 0
+chave = 'teste'
+
+while input('Descubra a palavra: ') != chave:
+    print("Ops que pena errou")
+
+
+for produto in pedido:
+    if(produto == 'teclado'):
+        pass
+        
+    print(produto)    
+
+# texto  = input('Digite aqui:   ')
+# print(texto)
